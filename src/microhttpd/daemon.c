8ec46971f0937d6ed4e933f4954fadb60e0f37a9
Christian Grothoff <christian@grothoff.org>
2018-08-18 11:22:08 +0200

    polish configure.ac, use AS_IF where appropriate

--- src/microhttpd/daemon.c
+++ src/microhttpd/daemon.c
@@ -3977,6 +3977,8 @@ MHD_poll (struct MHD_Daemon *daemon,
   return MHD_poll_listen_socket (daemon,
                                  may_block);
 #else
+  (void) daemon;
+  (void) may_block;
   return MHD_NO;
 #endif
 }
@@ -4014,29 +4016,24 @@ is_urh_ready(struct MHD_UpgradeResponseHandle * const urh)
        (0 == urh->in_buffer_used) &&
        (0 == urh->out_buffer_used) )
     return false;
-
   if (connection->daemon->shutdown)
     return true;
-
   if ( ( (0 != (MHD_EPOLL_STATE_READ_READY & urh->app.celi)) ||
          (connection->tls_read_ready) ) &&
        (urh->in_buffer_used < urh->in_buffer_size) )
     return true;
-
   if ( (0 != (MHD_EPOLL_STATE_READ_READY & urh->mhd.celi)) &&
        (urh->out_buffer_used < urh->out_buffer_size) )
     return true;
-
   if ( (0 != (MHD_EPOLL_STATE_WRITE_READY & urh->app.celi)) &&
        (urh->out_buffer_used > 0) )
     return true;
-
   if ( (0 != (MHD_EPOLL_STATE_WRITE_READY & urh->mhd.celi)) &&
          (urh->in_buffer_used > 0) )
     return true;
-
   return false;
 }
+
 
 /**
  * Do epoll()-based processing for TLS connections that have been
@@ -4147,10 +4144,12 @@ run_epoll_for_upgrade (struct MHD_Daemon *daemon)
 }
 #endif /* HTTPS_SUPPORT && UPGRADE_SUPPORT */
 
+
 /**
  * Pointer-marker to distinguish ITC slot in epoll sets.
  */
 static const char * const epoll_itc_marker = "itc_marker";
+
 
 /**
  * Do epoll()-based processing (this function is allowed to
